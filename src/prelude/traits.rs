13c88baa7f66970c22c49230b4704780562404f9
Lictor Guzman <lctrgzmn@gmail.com>
2021-12-30 02:59:14 -0600

    doc comments

--- src/prelude/traits.rs
+++ src/prelude/traits.rs
@@ -1,11 +1,17 @@
-/// Trait extending `Peekable<T>` functionality to any type I that behaves like (but is not required to be) an `Iterator<Item = T>`.
+/// Trait extending `Peekable<T>` functionality to any type `I` that
+/// behaves like (but is not required to be) an `Iterator<Item = T>`.
+///
 /// This trait *does not* require the base type to be an `Iterator`.
+/// However, every `Iterator<T>` can automatically produce a type
+/// whose `peek` method would not need to be implemented in order
+/// to implement `Peek`.
 pub trait Peek {
     type Item: std::cmp::PartialEq + Default;
 
     fn peek(&mut self) -> Option<&Self::Item>;
 
     fn is_done(&mut self) -> bool;
+
     fn match_curr(&mut self, item: &Self::Item) -> bool {
         if let Some(t) = self.peek() {
             item == t
@@ -17,9 +23,13 @@ pub trait Peek {
 
 /// Utility trait for structs following the `newtype` pattern.
 ///
-/// Since newtypes generally don't have their inner data publically accessible, it follows that relevant accessor methods must be implemented for each newtype, leading to a lot of boilerplate. This trait aims to standardize the accessor methods used on simple, single field tuple structs (aka, structs following the `newtype` design pattern).
+/// Since newtypes generally don't have their inner data publically
+/// accessible, it follows that relevant accessor methods must be
+/// implemented for each newtype, leading to a lot of boilerplate.
 ///
-/// While not all newtypes are used the same,
+/// This trait aims to standardize the accessor methods used on simple,
+/// single field tuple structs (aka, structs following the `newtype`
+/// design pattern).
 pub trait Newtype {
     /// The type of the element wrapped by the newtype.
     type Inner;

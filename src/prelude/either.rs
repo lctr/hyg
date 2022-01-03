c3b286035c892af6cfa9239c2aa4021b76f33502
Lictor Guzman <lctrgzmn@gmail.com>
2022-01-03 01:03:19 -0600

    added some option/result-like methods

--- src/prelude/either.rs
+++ src/prelude/either.rs
@@ -14,6 +14,22 @@ impl<L, R> Either<L, R> {
         !self.is_left()
     }
 
+    /// Given two closures, both of which have the same return type, applies
+    /// whichever one is compatible with the inner type, returning the
+    /// resolved value instead of an `Either` type.
+    pub fn resolve<F, G, X>(self, left: F, right: G) -> X
+    where
+        F: FnOnce(L) -> X,
+        G: FnOnce(R) -> X,
+    {
+        match self {
+            Either::Left(l) => left(l),
+            Either::Right(r) => right(r),
+        }
+    }
+
+    /// Consumes the inner value of type `L` and returns it as n `Option<L>`.
+    /// If `self` contained a value of type `R`, `None` is returned.
     pub fn take_left(self) -> Option<L> {
         if let Either::Left(l) = self {
             Some(l)
@@ -22,6 +38,8 @@ impl<L, R> Either<L, R> {
         }
     }
 
+    /// Consumes inner value of type `R` and returns it as an `Option<R>`.
+    /// If `self` contained a value of type `L`, `None` is returned.
     pub fn take_right(self) -> Option<R> {
         if let Either::Right(r) = self {
             Some(r)
@@ -30,6 +48,8 @@ impl<L, R> Either<L, R> {
         }
     }
 
+    /// Applies a function to the wrapped `Left` value, leaving the `Right`
+    /// value untouched.
     pub fn map_left<F, X>(self, f: F) -> Either<X, R>
     where
         F: FnOnce(L) -> X,
@@ -40,6 +60,8 @@ impl<L, R> Either<L, R> {
         }
     }
 
+    /// Applies a function to the wrapped `Right` value, leaving the `Left`
+    /// value untouched.
     pub fn map_right<F, Y>(self, f: F) -> Either<L, Y>
     where
         F: FnOnce(R) -> Y,
@@ -50,6 +72,7 @@ impl<L, R> Either<L, R> {
         }
     }
 
+    /// Returns an `Either` variant containing a reference to the inner value.
     pub fn as_ref(&self) -> Either<&L, &R> {
         match *self {
             Self::Left(ref l) => Either::Left(l),
@@ -57,6 +80,8 @@ impl<L, R> Either<L, R> {
         }
     }
 
+    /// Returns an `Either` variant containing a mutable reference to the inner
+    /// value.
     pub fn as_mut(&mut self) -> Either<&mut L, &mut R> {
         match *self {
             Self::Left(ref mut l) => Either::Left(l),
@@ -64,6 +89,8 @@ impl<L, R> Either<L, R> {
         }
     }
 
+    /// Consumes the inner value and returns it in the alternate `Either`
+    /// variant.
     pub fn transpose(self) -> Either<R, L> {
         match self {
             Self::Left(lr) => Either::Right(lr),
@@ -71,6 +98,33 @@ impl<L, R> Either<L, R> {
         }
     }
 
+    /// Makes a new copy of the inner value, returning it in the same `Either`
+    /// variant.
+    pub fn copied(&self) -> Either<L, R>
+    where
+        L: Copy,
+        R: Copy,
+    {
+        match self {
+            Either::Left(l) => Either::Left(*l),
+            Either::Right(r) => Either::Right(*r),
+        }
+    }
+
+    /// Clones the inner value and returns it in the same `Either` variant.
+    pub fn cloned(&self) -> Either<L, R>
+    where
+        L: Clone,
+        R: Clone,
+    {
+        match self {
+            Either::Left(l) => Either::Left(l.clone()),
+            Either::Right(r) => Either::Right(r.clone()),
+        }
+    }
+
+    /// Converts the inner `IntoIterator` value into an `Iterator`, returning
+    /// it wrapped in the corresponding `Either` variant.
     pub fn into_iter(
         self,
     ) -> Either<L::IntoIter, R::IntoIter>

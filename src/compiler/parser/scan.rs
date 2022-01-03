16513ed0d10e9d0bc9a3389e32dc649b4c6a5ee1
Lictor Guzman <lctrgzmn@gmail.com>
2022-01-03 00:59:02 -0600

    parsing by layout/indentation; off-side rule

--- src/compiler/parser/scan.rs
+++ src/compiler/parser/scan.rs
@@ -1,4 +1,6 @@
-use crate::prelude::{span::Positioned, traits::Peek};
+use crate::prelude::{
+    either::Either, span::Positioned, traits::Peek,
+};
 
 pub trait Consume<'t>: Peek + Positioned {
     type Error: std::fmt::Display;
@@ -11,6 +13,32 @@ pub trait Consume<'t>: Peek + Positioned {
 
     /// Returns the next item unwrapped. The underlying item to be iterated must be equivalently "null-terminated". By convention, this method should return the return value of `T::default()` if the `Option<&T>` returned by `peek` is `None`, otherwise it returns `T`,
     fn take_next(&mut self) -> Self::Peeked;
+
+    /// Consumes the next item and applies the provided closure to the
+    /// unerlying receiver along with the newly consumed item.
+    ///
+    /// Returns the result of the provided closure with a mutable reference
+    /// to the underlying container, wrapped in a `Result`.
+    ///
+    /// Equivalent to
+    /// ```
+    /// let next = self.take_next();
+    /// /* do something to `self` depending on `next` */
+    /// ```
+    ///  
+    fn with_next<F, X>(
+        &mut self,
+        mut f: F,
+    ) -> Result<X, Self::Error>
+    where
+        F: FnMut(
+            Self::Peeked,
+            &mut Self,
+        ) -> Result<X, Self::Error>,
+    {
+        let tok = self.take_next();
+        f(tok, self)
+    }
 
     // TODO: redo this, probably factor out error reporting.
     /// Generate an error describing the expected token, the actual token, and the position in the stream. The position is given in an `Option` type -- this is to allow flexibility in position reporting.
@@ -40,26 +68,6 @@ pub trait Consume<'t>: Peek + Positioned {
 /// produces a peekable ierator, for which the implementation of `Peek` is
 /// nearly trivial).
 pub trait Combinator<'t>: Consume<'t> {
-    /// Consumes the next item and applies the provided closure to the
-    /// unerlying receiver along with the newly consumed item.
-    ///
-    /// Returns the result of the provided closure with a mutable reference
-    /// to the underlying container, wrapped in a `Result`.
-    fn with_next<F, X>(
-        &mut self,
-        mut f: F,
-    ) -> Result<(X, &mut Self), Self::Error>
-    where
-        F: FnMut(
-            Self::Peeked,
-            &mut Self,
-        ) -> Result<X, Self::Error>,
-    {
-        let next = self.take_next();
-        let node = f(next, self)?;
-        Ok((node, self))
-    }
-
     /// Given a function `parse :: &mut Self -> Result<X, E>`, return the vector of nodes of type `X` obtained by applying `parse` repeatedly,
     /// initially after consuming a token matching `start` parameter, after
     /// each delimiter matching the `sep` parameter, and terminating upon
@@ -81,8 +89,8 @@ pub trait Combinator<'t>: Consume<'t> {
     where
         F: FnMut(&mut Self) -> Result<X, Self::Error>,
     {
-        self.eat(&start)?;
-        self.sep_by_until(sep, end, parse)
+        self.eat(&start)
+            .and_then(|p| p.sep_by_until(sep, end, parse))
     }
 
     /// Given a parser, a separator and an end delimiter, this applies
@@ -166,6 +174,9 @@ pub trait Combinator<'t>: Consume<'t> {
     /// Given a predicate and a parser, apply the parser
     /// repeatedly as long as the predicate returns `true`.
     ///
+    /// This method will always check the predicate prior to running
+    /// the given parser.
+    ///
     /// **Note:** the given predicate *must* be coercible to
     /// `fn` pointer, and hence **must not** capture any variables.
     fn many_while<F, X>(
@@ -232,27 +243,105 @@ pub trait Combinator<'t>: Consume<'t> {
         Ok(nodes)
     }
 
-    /// Attempts to consume `tok`, applying the parser `f` if successful and
-    /// returning its result in a `Some` variant.
-    ///
-    /// In theory, this method probably doesn't need to return a `Result`,
-    /// as it will only attempt to consume the given `tok` on a successful
-    /// match.
-    fn optional<F, X>(
+    /// Consunes and discards the specified token. Returns a mutable reference
+    /// to `self` for chaining.
+    fn ignore<F, X>(
         &mut self,
         tok: &Self::Peeked,
-        mut f: F,
-    ) -> Result<Option<X>, Self::Error>
+    ) -> &mut Self
     where
         F: FnMut(&mut Self) -> Result<X, Self::Error>,
     {
         if self.match_curr(tok) {
-            self.eat(tok)?;
-            f(self).map(|x| Some(x))
+            self.take_next();
+        }
+        self
+    }
+}
+
+pub trait Layout<'t>: Combinator<'t> {
+    /// Returns the `row` value of the given location IFF:
+    ///
+    /// * The `row` values of both the given and the current location are
+    ///   the same
+    /// * The `col` value of the current location is strictly *higher*
+    ///   than that of the given location
+    ///
+    /// Otherwise, it returns the `row` value of the current location.
+    ///
+    /// This is a utility method used to generate the base row value used
+    /// when parsing based on layout and an initial "effective" `row` value.
+    ///
+    fn fresh_row(&mut self, start: Self::Loc) -> u32 {
+        let start_row = start.get_row();
+        let curr_row = self.get_row();
+        if start_row == curr_row
+            || self.get_column() > start.get_column()
+        {
+            start_row
         } else {
-            Ok(None)
+            curr_row
         }
     }
 
-    // fn chain_left(&mut self) {}
+    /// Given a predicate, and a closure, this method uses position to
+    /// determine whether to apply the closure once, yielding an `Either::Left`
+    /// with the result, or repeatedly, yielding an `Either::Right` with the
+    /// first result and an array of the later results in a tuple.
+    ///
+    /// Assuming no errors were encountered, the above results are returned in
+    /// a `Result::Ok` variant, otherwise short-circuiting and returning an
+    /// error.
+    ///
+    /// This method will only repeat the closure provided as long as the
+    /// indentation after each application **exceeds** the original indentation
+    /// prior to applying the closure for the first time.
+    ///
+    ///
+    /// The aim of this method is to satisfy the `off-side` rule, wherein a
+    ///
+    fn within_offside<P, F, X>(
+        &mut self,
+        // start: Self::Loc,
+        pred: P,
+        mut parse: F,
+    ) -> Result<Either<X, (X, Vec<X>)>, Self::Error>
+    where
+        P: Fn(&Self::Peeked) -> bool,
+        F: FnMut(&mut Self) -> Result<X, Self::Error>,
+    {
+        let start = self.loc();
+        let start_row = self.get_row();
+        let start_col = self.get_column();
+        let head = parse(self)?;
+
+        if self.is_done()
+            || self
+                .peek()
+                .and_then(|t| Some(!pred(t)))
+                .unwrap_or_else(|| false)
+            || self.get_column() <= start_col
+            || self.get_row() > start_row
+        {
+            return Ok(Either::Left(head));
+        }
+
+        let mut nodes = vec![];
+        let mut row = self.fresh_row(start);
+
+        while !self.is_done()
+            && self.get_row() == row
+            && self
+                .peek()
+                .and_then(|t| Some(pred(t)))
+                .unwrap_or_else(|| false)
+        {
+            nodes.push(parse(self)?);
+            if self.get_column() > start_col {
+                row = self.get_row()
+            }
+        }
+
+        Ok(Either::Right((head, nodes)))
+    }
 }

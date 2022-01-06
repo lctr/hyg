19c98a8d8ce22d353a3e80ca2b6c55fe3cdc43dd
Lictor Guzman <lctrgzmn@gmail.com>
2022-01-06 10:07:03 -0600

    separated data types reliant on interned symbols

---
+++ src/compiler/syntax/name.rs
@@ -0,0 +1,46 @@
+use crate::{
+    compiler::lexer::TokenError, prelude::symbol::Symbol,
+};
+
+use super::{Operator, Token};
+
+#[derive(Clone, Debug, PartialEq, Eq, Hash)]
+pub enum Name {
+    /// Used for identifiers
+    Ident(Symbol),
+    /// Used for operators
+    Infix(Operator),
+    /// Used for data constructors
+    Cons(Symbol),
+    /// Used for classes -- play same syntactic role as TyCons,
+    /// but within constraints?
+    Class(Symbol),
+}
+
+impl std::fmt::Display for Name {
+    fn fmt(
+        &self,
+        f: &mut std::fmt::Formatter<'_>,
+    ) -> std::fmt::Result {
+        match self {
+            Name::Ident(s)
+            | Name::Cons(s)
+            | Name::Class(s) => {
+                write!(f, "{}", s)
+            }
+            Name::Infix(x) => write!(f, "{}", x),
+        }
+    }
+}
+
+impl std::convert::TryFrom<Token> for Name {
+    type Error = TokenError;
+    fn try_from(value: Token) -> Result<Self, Self::Error> {
+        match value {
+            Token::Lower(s) => Ok(Name::Ident(s)),
+            Token::Upper(s) => Ok(Name::Cons(s)),
+            Token::Operator(o) => Ok(Name::Infix(o)),
+            t => Err(TokenError::Incompatible(format!("Failed Token -> Var conversion! Expected either `Ident`, `Sym`, or `Operator` variant, but found {:?}", t)))
+        }
+    }
+}
